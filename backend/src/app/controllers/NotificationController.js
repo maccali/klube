b57254eed960bb61bbbd10da74d60efada26855a
maccali <guilherme.maccali@universo.univates.br>
2019-11-09 23:53:24 -0300

    First Commit

---
+++ backend/src/app/controllers/NotificationController.js
@@ -0,0 +1,37 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const notification = await Notification.findOneAndUpdate(
+      id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
