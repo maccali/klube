6adcebdb738b562f90a7efd3581d8cf2a1854412
maccali <guilherme.maccali@universo.univates.br>
2019-11-14 00:34:21 -0300

    docker compose ?

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,11 @@
 import app from './app';
 
-app.listen(3333);
+const PORT = 3333;
+const HOST = '0.0.0.0';
+
+console.log(`Rodando em modo  -->> ${process.env.NODE_ENV}`);
+console.log(`Rodando no endereço http://${process.env.HOST}:${process.env.PORT}`);
+
+app.listen(PORT, HOST);
+
+// app.listen(3333);
